2ccdc64f9e50224372887552f39fe616111ffa8a
Julian Israel De La Cruz <jdelacruz@hfmi.com>
2016-12-15 10:48:53 -0400

    fix for few commands

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,22 @@ var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 var browserify = require("browserify");
+var shell = require('gulp-shell');
 
-
-// compile ts files into js and move them to built folder
-function build(){
-     return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("built"));
-}
 
 // watch for file changes and rebuild
 function watch(){
-    gulp.watch('src/***.ts', [build]);
+    gulp.watch('src/***.ts', ["build"]);
 }
 
-
-
-gulp.task("build", build);
 gulp.task('watch',['build'], watch)
 
 gulp.task("default",["build"], function () {
 
 });
 
+gulp.task('build', shell.task([
+  'tsc'
+]));
 
 
