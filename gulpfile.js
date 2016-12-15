91ac38bb57daca4a94ec3de95c568e74442c758d
gmoyatorres <gmoyatorres@hfmi.com>
2016-12-15 12:11:03 -0400

    Added serve task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 var browserify = require("browserify");
 var shell = require('gulp-shell');
+var browserSync = require('browser-sync');
 
 
 // watch for file changes and rebuild
@@ -10,7 +11,21 @@ function watch(){
     gulp.watch('src/***.ts', ["build"]);
 }
 
-gulp.task('watch',['build'], watch)
+gulp.task('serve', ['build'], function (done) {
+    browserSync({
+    online: false,
+    port: 9000,
+    server: {
+      baseDir: ['.'],
+      middleware: function(req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+      }
+    }
+  }, done);
+})
+
+gulp.task('watch',['serve'], watch)
 
 gulp.task("default",["build"], function () {
 
