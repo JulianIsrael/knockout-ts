1558163476d3b7eb37ec9448cd47480799687a2a
Julian Israel De La Cruz <jdelacruz@hfmi.com>
2016-12-15 17:27:45 -0400

    blade index refactor

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,4 @@
 import * as ko from "knockout";
-import Blade1 from "./blades/blade1/index";
+import Blade1 from "./blades/blade1/main";
 
 ko.applyBindings(new Blade1({ counter: 1 } ), document.getElementById('app'));
