3faa65cb34812ba92f6a7ecbfbd4bfafa8eebf61
Julian Israel De La Cruz <jdelacruz@hfmi.com>
2016-12-15 11:02:22 -0400

    reference to knockout added

--- src/require-config.ts
+++ src/require-config.ts
@@ -1,6 +1,6 @@
 declare var require: any;
 require.config({
     paths: {
-        "knockout": "externals/knockout-3.4.0",
+        "knockout": "node_modules/knockout/build/output/knockout-latest",
     }
 });
